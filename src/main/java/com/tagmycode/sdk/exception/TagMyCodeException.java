6efcac83b4a750489caf5762a8b67c5840831120
Massimo Zappino <massimo.zappino@gmail.com>
2017-03-30 14:12:41 +0200

    refactoring pom removed default TagMyCodeException constructor

--- src/main/java/com/tagmycode/sdk/exception/TagMyCodeException.java
+++ src/main/java/com/tagmycode/sdk/exception/TagMyCodeException.java
@@ -2,18 +2,13 @@ package com.tagmycode.sdk.exception;
 
 public class TagMyCodeException extends Exception {
 
-    public TagMyCodeException() {
-        super("Unknown error");
-    }
-
     public TagMyCodeException(String message) {
         super(message);
     }
 
     public TagMyCodeException(Exception e) {
-        super(e.getMessage());
+        super(e);
     }
-
 
     @Override
     public String toString() {
